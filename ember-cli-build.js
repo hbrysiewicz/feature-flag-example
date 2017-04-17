6f929889ef782328a0f1a95c068aacee443d9e7b
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2017-04-17 12:09:05 -0700

    Setup feature-flag addon and build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,40 @@
 /* eslint-env node */
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
+const { toConst } = require('./lib/utils/features')
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+  let env = EmberApp.env() || 'development'
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  let isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1
+  let features = require('./features').features
 
-  return app.toTree();
-};
+  let babel = { plugins: [] }
+
+  // Dead code elimination in production
+  if (isProductionLikeBuild) {
+    babel.plugins.push(['minify-dead-code-elimination', { 'optimizeRawSize': true }])
+  }
+
+  let app = new EmberApp(defaults, {
+    babel,
+
+    'ember-cli-babel': {
+      includePolyfill: true
+    },
+
+    'ember-cli-qunit': {
+      useLintTree: false
+    },
+
+    fingerprint: {
+      enabled: false
+    },
+
+    sourcemaps: {
+      enabled: !isProductionLikeBuild,
+      extensions: ['js']
+    }
+  })
+
+  return app.toTree()
+}
